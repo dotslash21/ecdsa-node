a6267f64d6e0fbc0997246108e15c9a6b58ad63e
Arunangshu Biswas <arunangshubsws@gmail.com>
2023-04-20 08:13:52 +0530

    feat: Use private key to query balance

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,9 +1,25 @@
 import server from "./server";
+import { secp256k1 } from "ethereum-cryptography/secp256k1";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { toHex, hexToBytes } from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({
+  address,
+  setAddress,
+  balance,
+  setBalance,
+  privateKey,
+  setPrivateKey,
+}) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+
+    const publicKey = secp256k1.getPublicKey(hexToBytes(privateKey));
+
+    const address = toHex(keccak256(publicKey.slice(1)).slice(12));
     setAddress(address);
+
     if (address) {
       const {
         data: { balance },
@@ -19,9 +35,15 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input
+          placeholder="Type in a private key"
+          value={privateKey}
+          onChange={onChange}
+        ></input>
       </label>
+
+      <div className="">Address: {address}</div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
--- server/index.js
+++ server/index.js
@@ -7,9 +7,17 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  // Private key: b755060cd5967d500ba0d9377a8e36bf53d569bd764bcf7b5cce12d7dc711e29
+  // Public key: 02a694bfb6a33ae81703abb0d5923b4d35afb06d3af26602de7eef24c9f15a0e92
+  bec0d590ac2872d1e89396e49c14016303076009: 100,
+
+  // Private key: 11a1927c9c407fb552b03f43f1d715bb7da1be92d09939114c9ff84bfe7c4d90
+  // Public key: 03225b7f566eba642e821761a1ccea2bdb69ccc09b38f9f5db866a79d76d16e1eb
+  e12492c20b6dc8a3dddde7f59f8d5f54ce636941: 50,
+
+  // Private key: ad2e8ccfcbee5aa47d22f6246755650673cf22f06f2a4d402c27be948c1e2624
+  // Public key: 02036d0d1a793785cceed7f9e23f89f9bb0e6ef905361ab65682e9e406d1467484
+  "012717a22a9cf397e5403afba1bb5fa13539af62": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,6 +27,8 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  // TODO: Get a signature from the sender and verify it
+
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
